af9d358ea33996de0e51341f36a191d4250447ad
Denis Coelho <deniscoelho.dev@gmail.com>
2026-09-11 18:12:56 -0300

    feat: atualizando progjuros

--- PROGJUROSCOB.cbl
+++ PROGJUROSCOB.cbl
@@ -18,6 +18,7 @@
        77 WS-TAXA-JUROS            PIC 99,999.
        77 WS-PERIODO               PIC 999.
        77 WS-JUROS-SIMPLES         PIC $9(5),99.
+       77 WS-JUROS-COMPOSTOS       PIC $9(5),99.
        77 WS-MONTANTE-SIMPLES      PIC $9(5),99.
        77 WS-MONTANTE-COMPOSTO     PIC $9(5),99.
 
@@ -31,8 +32,6 @@
 
        77 WS-RESPOSTA              PIC X(1).
 
-      *    https://www.youtube.com/watch?v=SxSNgXZsLgE  20:31
-
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
            DISPLAY "===============================================".
@@ -41,11 +40,56 @@
            DISPLAY "INFORME O VALOR PRINCIPAL (R$): " WITH NO ADVANCING.
            ACCEPT WS-VALOR-PRINCIPAL.
            MOVE WS-VALOR-PRINCIPAL TO WS-VP-NUM.
-
            DISPLAY "INFORME A TAXA DE JUROS (% AO MES): "
                                                       WITH NO ADVANCING.
            ACCEPT WS-TAXA-JUROS.
            MOVE WS-TAXA-JUROS TO WS-TJ-NUM.
+
+           DISPLAY "INFORM O PERIODO EM (MESES): " WITH NO ADVANCING.
+
+           ACCEPT WS-PERIODO.
+           MOVE WS-PERIODO TO WS-PERIODO-NUM.
+
+      *CALULO DE JUROS SIMPLES
+       COMPUTE WS-JS-NUM = WS-VP-NUM * (WS-TJ-NUM / 100) *
+                                           WS-PERIODO-NUM.
+       COMPUTE WS-MS-NUM = WS-VP-NUM + WS-JS-NUM.
+
+       MOVE WS-JS-NUM TO WS-JUROS-SIMPLES.
+       MOVE WS-MS-NUM TO WS-MONTANTE-SIMPLES.
+
+      *CALULO DE JUROS COMPOSTOS
+       COMPUTE WS-MC-NUM = WS-VP-NUM * (1 + (WS-TJ-NUM / 100))
+                                                  ** WS-PERIODO-NUM.
+       COMPUTE WS-JC-NUM = WS-MC-NUM - WS-VP-NUM.
+
+       MOVE WS-JC-NUM TO WS-JUROS-COMPOSTOS.
+       MOVE WS-MC-NUM TO WS-MONTANTE-COMPOSTO.
+
+      *EXIBIR OS RESULTADOS
+       DISPLAY " ".
+       DISPLAY "                   RESULTADOS                         ".
+       DISPLAY "======================================================".
+       DISPLAY "                  JUROS SIMPLES                       ".
+       DISPLAY "======================================================".
+       DISPLAY "JUROS: R " FUNCTION TRIM(WS-JUROS-SIMPLES).
+       DISPLAY "MONTANTE: R " FUNCTION TRIM(WS-MONTANTE-SIMPLES).
+       DISPLAY " ".
+       DISPLAY "                   RESULTADOS                         ".
+       DISPLAY "======================================================".
+       DISPLAY "                  JUROS COMPOSTOS                     ".
+       DISPLAY "======================================================".
+       DISPLAY "JUROS: R " FUNCTION TRIM(WS-JUROS-COMPOSTOS).
+       DISPLAY "MONTANTE: R " FUNCTION TRIM(WS-MONTANTE-COMPOSTO).
+       DISPLAY " ".
+       DISPLAY "DESEJA REALIZAR NOVO CALCULO? " WITH NO ADVANCING.
+       ACCEPT WS-RESPOSTA.
+
+       IF WS-RESPOSTA = "S" OR "s"
+           GO TO MAIN-PROCEDURE
+       ELSE
+           DISPLAY " ".
+           DISPLAY "OBRIGADO POR USAR O SISTEMA".
 
             STOP RUN.
 
